3d9005c782d54302565854cc08396312a67cf0a9
omarmiatello <jackl85@gmail.com>
2019-02-09 21:17:30 +0100

    remove sdkman

--- sh/install.sh
+++ sh/install.sh
@@ -2,10 +2,10 @@
 
 if [ ! -d "/app/.sdkman" ]; then
   echo 'installing sdkman and kotlin'
-  curl -s \"https://get.sdkman.io\" | bash
-  source \"/app/.sdkman/bin/sdkman-init.sh\"
-  sdk install kotlin
-  sdk flush archives
+#  curl -s \"https://get.sdkman.io\" | bash
+#  source \"/app/.sdkman/bin/sdkman-init.sh\"
+#  sdk install kotlin
+#  sdk flush archives
 else
   echo 'sdkman installed; assuming kotlin is as well'
 fi
